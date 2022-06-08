66c99da09901aff1bb18ac3c8c2978472b12e7c1
rnjstmdals6 <rnjstmdals6@naver.com>
2022-06-08 17:41:11 +0900

    TEST 소셜로그인
    소셜로그인 테스트용도로 머지합니다.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import Banner from '../components/banner/Banner';
 import Footer from '../components/footer/Footer';
 import List from '../components/post/List';
+import Socical from '../components/social/social';
 
 function MainPage() {
   return (
     <>
+      <Socical />
       <Banner />
       <List />
       <Footer />
